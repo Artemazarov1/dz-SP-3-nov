04d620769dfe2c8bd33cbfa77637ef9888ce5ed1
Artemazarov1 <146062675+Artemazarov1@users.noreply.github.com>
2023-11-03 09:20:31 +0300

    Add files via upload

---
+++ 2 zadanie 3 nov.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum(int n) 
+{
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10; // получаем последнюю цифру числа и добавляем ее к сумме
+        n /= 10; // удаляем последнюю цифру из числа
+    }
+    return sum;
+}
+
+int more(int n) 
+{
+    while (n >= 10) {
+        n = sum(n);
+    }
+    return n;
+}
+
+int main() 
+{
+    setlocale(0, "");
+    int number;
+    cout << "введите число: ";
+    cin >> number;
+
+    int result = more(number);
+    cout << "итоговое однозначное число: " << result << endl;
+
+    return 0;
+}
---
+++ dz 3 noyabria.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void zadanieArray(int array[], int size) 
+{
+    
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = rand() % 100;
+    }
+}
+
+void pokazArray(int array[], int size) 
+{
+    for (int i = 0; i < size; i++) 
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    setlocale(0, "");
+    const int size1 = 5;
+    const int size2 = 7;
+
+    int array1[size1];
+    int array2[size2];
+
+    zadanieArray(array1, size1);
+    zadanieArray(array2, size2);
+
+    cout << "Массив номер 1: ";
+    pokazArray(array1, size1);
+
+    cout << "Массив номер 2: ";
+    pokazArray(array2, size2);
+    
+    return 0;
+}
